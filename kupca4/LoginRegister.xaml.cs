0e300b30ddb03e8dc531018bcbbc49a90d54a713
Andrei <v.andreihaneckii@gmail.com>
2021-04-23 18:50:48 +0300

    готов дизайн логина и главного окна

---
+++ kupca4/LoginRegister.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace kupca4
+{
+    public partial class LoginRegister : Window
+    {
+        public LoginRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ kupca4/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace kupca4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
