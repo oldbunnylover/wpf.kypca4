72bdc186f6f254638cea164f8a663c6002cc7476
Andrei <v.andreihaneckii@gmail.com>
2021-04-26 10:04:32 +0300

    реализованы команды для кнопок главного окна. фикс maximize главного окна

---
+++ kupca4/Helpers/MaximizeFix.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace kupca4.Helpers
+{
+    public static class MaximizeFix
+    {
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("user32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        public static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam, int minWidth, int minHeight)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = MaximizeFix.MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
+            {
+
+                MaximizeFix.MONITORINFO monitorInfo = new MaximizeFix.MONITORINFO();
+                MaximizeFix.GetMonitorInfo(monitor, monitorInfo);
+                MaximizeFix.RECT rcWorkArea = monitorInfo.rcWork;
+                MaximizeFix.RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
+                mmi.ptMinTrackSize.x = minWidth;
+                mmi.ptMinTrackSize.y = minHeight;
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {            
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+          
+            public RECT rcMonitor = new RECT();
+           
+            public RECT rcWork = new RECT();
+          
+            public int dwFlags = 0;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public static readonly RECT Empty = new RECT();
+
+            public int Width
+            {
+                get { return Math.Abs(right - left); } 
+            }
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+            public RECT(RECT rcSrc)
+            {
+                this.left = rcSrc.left;
+                this.top = rcSrc.top;
+                this.right = rcSrc.right;
+                this.bottom = rcSrc.bottom;
+            }
+
+            public bool IsEmpty
+            {
+                get
+                {
+                    return left >= right || top >= bottom;
+                }
+            }
+            public override string ToString()
+            {
+                if (this == RECT.Empty) { return "RECT {Empty}"; }
+                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Rect)) { return false; }
+                return (this == (RECT)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
+            }
+
+            public static bool operator ==(RECT rect1, RECT rect2)
+            {
+                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
+            }
+
+            public static bool operator !=(RECT rect1, RECT rect2)
+            {
+                return !(rect1 == rect2);
+            }
+        }
+    }
+}
--- kupca4/ViewModels/MainWindowViewModel.cs
+++ kupca4/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using kupca4.DB;
+using kupca4.Helpers.Commands;
 using kupca4.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace kupca4.ViewModels
 {
@@ -8,23 +11,54 @@ namespace kupca4.ViewModels
         #region private fields
 
         private User user;
-        private string _title;
+        private WindowState _windowState = WindowState.Normal;
         #endregion
 
-        #region private fields
+        #region public fields
 
-        public string title { get => user.Fullname; }
-
-        #endregion
-
-        public MainWindowViewModel()
+        public WindowState windowState
         {
-
+            get => _windowState;
+            set => Set(ref _windowState, value);
         }
+
+        public string test
+        {
+            get => user.Fullname;
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand WindowMinimizedCommand { get; }
+        private bool CanWindowMinimizedCommandExecute(object p) => true;
+        private void OnWindowMinimizedCommandExecuted(object p) => windowState = WindowState.Minimized;
+
+        public ICommand WindowMaximizeCommand { get; }
+        private bool CanWindowMaximizeCommandExecute(object p) => true;
+        private void OnWindowMaximizeCommandExecuted(object p)
+        {
+            windowState = windowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        public ICommand SwitchUserCommand { get; }
+        private bool CanSwitchUserCommandExecute(object p) => true;
+        private void OnSwitchUserCommandExecuted(object p)
+        {
+            var LoginRegister = new LoginRegister();
+            LoginRegister.Show();
+            Application.Current.MainWindow.Close();
+        }
+
+        #endregion
 
         public MainWindowViewModel(User user)
         {
             this.user = user;
+
+            WindowMinimizedCommand = new LambdaCommand(OnWindowMinimizedCommandExecuted, CanWindowMinimizedCommandExecute);
+            WindowMaximizeCommand = new LambdaCommand(OnWindowMaximizeCommandExecuted, CanWindowMaximizeCommandExecute);
+            SwitchUserCommand = new LambdaCommand(OnSwitchUserCommandExecuted, CanSwitchUserCommandExecute);
         }
     }
 }
